c4e77bd11b26a6742984c4ccfa59f8d3cb0a13d1
Yannic Rieger <riegeryannic@gmail.com>
2018-12-26 17:58:08 +0100

    Fix timeout test

--- pkg/raft/timeout_test.go
+++ pkg/raft/timeout_test.go
@@ -1,8 +1,6 @@
 package raft
 
 import (
-	"fmt"
-	"github.com/luxordynamics/luxor/pkg/raft/protocol/out"
 	"testing"
 )
 
@@ -13,19 +11,7 @@ func Test_SetTimeoutConfig_Successful(t *testing.T) {
 }
 
 func Test_SetTimeoutConfig_With_Smaller_Values(t *testing.T) {
-	/*
 	if err := SetTimeoutConfig(0, -3); err == nil {
 		t.Fail()
-	}*/
-
-	var l interface{}
-	l = raft.AppendEntriesRequest{}
-
-	switch v := l.(raft.AppendEntriesRequest) {
-	default:
-		fmt.Printf("unexpected type %T", v)
-	case raft.AppendEntriesRequest:
-		fmt.Println("lol")
 	}
-	
 }
