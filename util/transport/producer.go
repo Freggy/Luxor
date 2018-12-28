fbfa6094a1e1cc09daf3ae6460dc130f95a0e18f
Yannic Rieger <riegeryannic@gmail.com>
2018-12-28 21:59:08 +0100

    Use not broken version of protobuf, v1.2.0 seems not to work with proto3

--- util/transport/producer.go
+++ util/transport/producer.go
@@ -1,9 +1,11 @@
 package transport
 
+import "github.com/luxordynamics/luxor/pkg/raft/protocol/gen"
+
 type Producer interface {
 
 	// Sends the given packet over the wire
-	Produce(<-chan Packet)
+	Produce(<-chan raft.Packet)
 
 	// Closes the connection and frees all resources.
 	Close()
