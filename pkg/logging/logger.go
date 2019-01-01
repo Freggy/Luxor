f7be78cdf544c48c31d818f794f00f2eca11d76b
freggy <riegeryannic@gmail.com>
2019-01-01 23:43:42 +0100

    Start work on logging package

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -2,8 +2,17 @@ package logging
 
 import "github.com/sirupsen/logrus"
 
+func newDebugLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetLevel(logrus.DebugLevel)
+	return logger
+}
+
 func newProductionLogger() *logrus.Logger {
 	logger := logrus.New()
-
+	logger.SetLevel(logrus.InfoLevel)
+	// TODO: implement the following options:
+	// - Use JSON logger, if configured
+	// - Use logstash logger, if configured
 	return logger
 }
