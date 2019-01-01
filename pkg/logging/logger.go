fca169cae21efa59493a138e3c0df6c45b09c053
freggy <riegeryannic@gmail.com>
2019-01-01 23:24:30 +0100

    Remove zap and add logrus

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -1,56 +1,9 @@
 package logging
 
-import (
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"io/ioutil"
-	"os"
-	"strings"
-)
+import "github.com/sirupsen/logrus"
 
-const (
-	Debug      = "DEBUG"
-	Production = "PRODUCTION"
-)
+func newProductionLogger() *logrus.Logger {
+	logger := logrus.New()
 
-func LoggerFromString(str string) *zap.Logger {
-	if strings.EqualFold(Debug, str) {
-		return nil
-	} else if strings.EqualFold(Production, str) {
-		return newProductionLogger()
-	} else {
-		return nil // TODO: return debug logger when invalid log type
-	}
-}
-
-func newProductionLogger() *zap.Logger {
-
-	// TODO: use custom logger config
-
-	infoLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl == zapcore.InfoLevel
-	})
-
-	warnLevelAndAbove := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.WarnLevel
-	})
-
-	jsonInfo := zapcore.AddSync(ioutil.Discard)
-	jsonErrors := zapcore.AddSync(ioutil.Discard)
-
-	consoleInfo := zapcore.Lock(os.Stdout)
-	consoleErrors := zapcore.Lock(os.Stderr)
-
-	jsonEncoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
-	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
-
-	core := zapcore.NewTee(
-		zapcore.NewCore(jsonEncoder, jsonErrors, warnLevelAndAbove),
-		zapcore.NewCore(jsonEncoder, jsonInfo, infoLevel),
-
-		zapcore.NewCore(consoleEncoder, consoleErrors, warnLevelAndAbove),
-		zapcore.NewCore(consoleEncoder, consoleInfo, infoLevel),
-	)
-
-	return zap.New(core)
+	return logger
 }
